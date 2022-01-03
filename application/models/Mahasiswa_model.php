42e8421f420f3247e7f3570acc56b20eb96023ef
Zaint305 <zeventien305@gmail.com>
2022-01-03 07:14:29 +0700

    first commit

---
+++ application/models/Mahasiswa_model.php
@@ -0,0 +1,101 @@
+<?php 
+use GuzzleHttp\Client;
+
+class Mahasiswa_model extends CI_model {
+
+    private $_client;
+
+    public function __construct()
+    {
+        $this->_client = new Client([
+            'base_uri' => 'http://localhost:8080/Mhs_Rest_Server/index.php/'
+        ]);
+    }
+
+    public function getAllMahasiswa()
+    {
+        $response = $this->_client->request('GET', 'mahasiswa',[
+            'query' => [
+                'API-KEY' => '1234'
+            ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        return $result['data'];
+    }
+
+    public function tambahDataMahasiswa()
+    {
+        $data = [
+            "nama" => $this->input->post('nama', true),
+            "nrp" => $this->input->post('nrp', true),
+            "email" => $this->input->post('email', true),
+            "jurusan" => $this->input->post('jurusan', true),
+            'API-KEY' => '1234'
+        ];
+
+        $response = $this->_client->request('POST', 'mahasiswa', [
+            'from_params' => $data
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result;
+    }
+
+    public function hapusDataMahasiswa($id)
+    {
+        $response = $this->_client->request('DELETE', 'mahasiswa', [
+            'form_params' => [
+                'id' => $id,
+                'API-KEY' => '1234'
+            ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result;
+    }
+
+    public function getMahasiswaById($id)
+    {
+        $response = $this->_client->request('GET', 'mahasiswa',[
+            'query' => [
+                'API-KEY' => '1234',
+                'id' => $id
+            ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        return $result['data'][0];
+    }
+
+    public function ubahDataMahasiswa()
+    {
+        $data = [
+            "nama" => $this->input->post('nama', true),
+            "nrp" => $this->input->post('nrp', true),
+            "email" => $this->input->post('email', true),
+            "jurusan" => $this->input->post('jurusan', true),
+            "id" => $this->input->post('id', true),
+            'API-KEY' => '1234'
+        ];
+
+        $response = $this->_client->request('PUT', 'mahasiswa', [
+            'from_params' => $data
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result;
+    }
+
+    public function cariDataMahasiswa()
+    {
+        $keyword = $this->input->post('keyword', true);
+        $this->db->like('nama', $keyword);
+        $this->db->or_like('jurusan', $keyword);
+        $this->db->or_like('nrp', $keyword);
+        $this->db->or_like('email', $keyword);
+        return $this->db->get('mahasiswa')->result_array();
+    }
+}
